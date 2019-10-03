ff9dab69a6bf8ce16e2ca5128cecc2292b8523b9
Seth Maisel <seth.maisel@gmail.com>
2019-10-03 17:55:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute_name, attributes|
     attributes.each do |attribute_value, pigeon_name|
       pigeon_name.each do |name|
-        pigeon_list[name]  = {}
+        pigeon_list[name] || = {}
+        
       
       end
     end
